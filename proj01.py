dc8b79de413df53be0def37ebe7c1cf7e1db8a46
Justin Qassis <juqassis@microsoft.com>
2022-11-22 23:35:57 -0500

    Update proj01.py

--- proj01.py
+++ proj01.py
@@ -1,3 +1,5 @@
+import math
+
 print ("welcome to Horizons Car Rentals ") 
 print("At the prompts, please enter the following:") 
 print("Customer's classification code (a character: BD, D, W)")
@@ -10,26 +12,34 @@ print("Odometer reading at the end of the rental period (int)")
 
 doesUserContinue = input("do you want to continue?: ")
 baseCharge = 40.00
+while doesUserContinue != "B":
+    print("That is not a valid option, please try again")
+    doesUserContinue = input("do you want to continue?: ")
+    while doesUserContinue == "A":
+            customerCode = input("Customer code(BD, D, W): ")
+            numDays = int(input("Number of days: "))
+            beginningOdometerReading = int(input("Odometer reading at  start: "))
+            endingOdometerReading = int(input("reading at the end:"))
+            roundedMileage = (endingOdometerReading - beginningOdometerReading) /10
+            if customerCode == "BD":
+                baseChargeTimesDays = baseCharge * numDays
+                newPrice =  baseChargeTimesDays+ (roundedMileage * 0.25)
+                print(newPrice)
+                doesUserContinue = input("do you want to continue?: ")
+            elif customerCode == "D":
+                baseCharge = 60.00
+                if roundedMileage / numDays > 100:
+                    allowedMiles = numDays * 100
+                    overageCharge = roundedMileage - allowedMiles
+                    totalCharge = baseCharge * numDays + ( overageCharge * 0.25)
+                    print(totalCharge)
 
-while doesUserContinue == "A":
-        customerCode = input("Customer code(BD, D, W): ")
-        numDays = int(input("Number of days: "))
-        beginningOdometerReading = float(input("Odometer reading at  start: "))
-        endingOdometerReading = float(input("reading at the end:"))
-        
-        if customerCode == "BD":
-            beginningOdometerReadingRounded = round(beginningOdometerReading, 1)
-            endingOdometerReadingRounded = round(endingOdometerReading,1)
-            baseChargeTimesDays = baseCharge * numDays
-            newPrice =  baseChargeTimesDays+ (endingOdometerReading - beginningOdometerReading * 0.25)
-            print(newPrice)
-            doesUserContinue = input("do you want to continue?: ")
-        elif customerCode == "D":
-            print("You selected D")
-            doesUserContinue = input("do you want to continue?: ")
-        elif customerCode == "W":
-            print("You selected W")
-            doesUserContinue = input("do you want to continue?: ")
+
+                doesUserContinue = input("do you want to continue?: ")
+            elif customerCode == "W":
+                print("You selected W")
+                #code here 
+                doesUserContinue = input("do you want to continue?: ")
 
             
 
